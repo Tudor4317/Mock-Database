f6c01647afc760a2991a6f06a7ed92a9a2d3ba50
Tudor <tudorfrincu@proton.me>
2026-05-06 12:37:12 -0400

    Final code commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,15 @@
 import mockDatabase from "../storages/usersStorage.js"
+import {body, validationResult,matchedData } from "express-validator"
+const alphaErr = "must only contain letters. "
+const lengthErr = "must be between 1 and 10 charactes."
+const validateUser =  [
+    body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({min: 1,max:10}).withMessage(`First name ${lengthErr}`),
+    body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({min:1, max:10}).withMessage(`Last name ${lengthErr}`)
+]
 
 export function getUsers(req,res) {
 
@@ -6,14 +17,50 @@ res.render("index", {title: "User List ",users : mockDatabase.getUsers()})
 
 }
 
+export const postCreate = [
+    validateUser,
+    (req,res) =>{
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).render("createUser",{ 
+                title: "Create user",
+                errors: errors.array()
+
+
+            });
+        }
+        const {firstName,lastName} = matchedData(req)
+        mockDatabase.addUser({firstName,lastName})
+        res.redirect("/")
+    }
+]
 
 export function getCreate (req,res){
-    res.render("createUser",{title:"Create User"})
+    res.render("createUser",{title:"Create User",errors: []
+    })
 }
 
-export function postCreate(req,res){
-    const {firstName, lastName} = req.body
-    mockDatabase.addUser({firstName, lastName})
+export function getUpdate(req,res){
+    const user = mockDatabase.getUsers(req.params.id)
+    res.render("updateUser",{title:"Update user", user: user, errors: []})
+}
+export const postUpdate = [
+    validateUser,
+    (req,res) =>{
+        const user = mockDatabase.getUsers(req.params.id)
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).render("updateUser",{title: "Update user", user: user, errors: errors.array()})
+
+        }
+        const {firstName, lastName} = matchedData(req)
+        mockDatabase.updateUser(req.params.id, {firstName,lastName})
+        res.redirect("/")
+    }
+]
+export function deletePost(req,res){
+    mockDatabase.deleteUser(req.params.id)
     res.redirect("/")
+
 }
 
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,10 +2,15 @@ import {Router} from "express"
 import { getCreate } from "../controllers/usersController.js"
 import { postCreate } from "../controllers/usersController.js"
 import { getUsers } from "../controllers/usersController.js"
+import { getUpdate } from "../controllers/usersController.js"
+import { postUpdate } from "../controllers/usersController.js"
+import { deletePost } from "../controllers/usersController.js"
 const usersRouter = Router()
 
 usersRouter.get("/",getUsers)
 usersRouter.get("/create", getCreate)
 usersRouter.post("/create",postCreate)
-
+usersRouter.get("/:id/update",getUpdate)
+usersRouter.post("/:id/update",postUpdate)
+usersRouter.post("/:id/delete",deletePost)
 export default usersRouter 
